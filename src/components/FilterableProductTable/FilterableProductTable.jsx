2f74020b7665e2c3ad8624e021709d2905ee2dd4
Roberto Costa <romeromatallica@gmail.com>
2022-06-13 04:59:48 -0300

    add useState in FilterableProductTable, SearchBar and ProductTable

--- src/components/FilterableProductTable/FilterableProductTable.jsx
+++ src/components/FilterableProductTable/FilterableProductTable.jsx
@@ -1,12 +1,33 @@
 import { FilterableProductTablePage } from "./FilterableProductTable.styled";
 import { SearchBar } from "../SearchBar";
 import { ProductTable } from "../ProductTable";
+import { useState } from "react";
 
 export const FilterableProductTable = ({ products }) => {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  const onFilterTextChange = ({ target: { value } }) => {
+    setFilterText(value);
+  };
+
+  const onInStockOnlyChange = ({ target: { checked } }) => {
+    setInStockOnly(checked);
+  };
+
   return (
     <FilterableProductTablePage>
-      <SearchBar />
-      <ProductTable products={products} />
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={onFilterTextChange}
+        onInStockOnlyChange={onInStockOnlyChange}
+      />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
     </FilterableProductTablePage>
   );
 };
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,25 @@
 import { SearchBarForm, InputSearch } from "./SearchBar.styled";
 
-export const SearchBar = () => {
+export const SearchBar = ({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange,
+}) => {
   return (
     <SearchBarForm>
-      <InputSearch type="text" placeholder="Search..." />
+      <InputSearch
+        type="text"
+        placeholder="Search..."
+        value={filterText}
+        onChange={onFilterTextChange}
+      />
       <label>
-        <input type="checkbox" /> Only show products in stock
+        <input
+          type="checkbox"
+          onChange={onInStockOnlyChange}
+        />{" "}
+        Only show products in stock
       </label>
     </SearchBarForm>
   );
